2ae9676ac873d44819559d448edc6f64ca0dd0d8
Piotr Piechota <piotr_piechota@poczta.onet.pl>
2020-02-03 21:12:53 +0100

    Initial commit

---
+++ src/main/java/pl/piotrpiechota/nbahighlightsfinder/NbaHighlightsFinderApplication.java
@@ -0,0 +1,13 @@
+package pl.piotrpiechota.nbahighlightsfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaHighlightsFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbaHighlightsFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/piotrpiechota/nbahighlightsfinder/NbaHighlightsFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.piotrpiechota.nbahighlightsfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaHighlightsFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
